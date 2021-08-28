21c65e3fcbddcf9b2ea119019d65a0490c881385
Tomoya Mikami <trymorecc@gmail.com>
2021-08-28 23:47:41 +0900

    nyaaaan!!!

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,54 @@
 import * as React from "react";
 import { Style } from "./style";
-import { Text, View, StyleSheet, Button, Image } from "react-native";
-import { Video, AVPlaybackStatus } from "expo-av";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Audio } from "expo-av";
 import { Camera } from "expo-camera";
 import * as FaceDetector from "expo-face-detector";
 import { useStopwatch, useTimer } from "react-timer-hook";
 
 export default function App() {
-  const video = React.useRef(null);
   const camera = React.useRef<Camera>(null);
-  const [status, setStatus] = React.useState({});
+  const [sound, setSound] = React.useState<Audio.Sound | undefined>(undefined);
   const { seconds, reset } = useStopwatch({ autoStart: true });
+  const [firstCrying, setFirstCrying] = React.useState(true);
   const [hasPermission, setHasPermission] = React.useState<null | boolean>(
     null
-  );
-  const [annotationLabelText, setAnnotationLabelText] = React.useState<string>(
-    ""
   );
 
   const onFacesDetected = () => {
     reset();
   };
 
+  const isCatAppearance = (): boolean => seconds > 3;
+
   React.useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
       setHasPermission(status === "granted");
+      const soundSetUp = new Audio.Sound();
+          await soundSetUp.loadAsync(require("../assets/cat1b.mp3"));
+          setSound(soundSetUp);
     })();
   }, []);
+
+  React.useEffect(() => {
+    if (!isCatAppearance() && !firstCrying) {
+      setFirstCrying(true);
+    }
+    if (sound !== undefined && isCatAppearance()) {
+      (async () => {
+        const soundStatus = await sound.getStatusAsync();
+        if (soundStatus.isLoaded && !soundStatus.isPlaying && firstCrying) {
+          try {
+              setFirstCrying(false);
+              sound.replayAsync();
+            } catch (error) {
+              console.log(error);
+            }
+        }
+      })();
+    }
+  }, [seconds]);
 
   if (hasPermission === null) {
     return <View />;
@@ -51,7 +72,7 @@ export default function App() {
       type={Camera.Constants.Type.front}
       ref={camera}
     >
-      {seconds > 3 && <Image source={require("../assets/cat_motion_1.gif")} />}
+      {isCatAppearance() && <Image source={require("../assets/cat_1.gif")} />}
     </Camera>
   );
 }
